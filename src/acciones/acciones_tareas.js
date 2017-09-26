4d3afc0393483a8a8a8f579e48f25e3125153b2c
Jonathan Castro <jonatancastro1@gmail.com>
2017-09-26 17:45:06 -0500

    Adelanto acciones de tareas

---
+++ src/acciones/acciones_tareas.js
@@ -0,0 +1,26 @@
+export const AGREGAR_TAREA = "AGREGAR_TAREA";
+export const EDITAR_TAREA = "EDITAR_TAREA";
+export const ELIMINAR_TAREA = "ELIMINAR_TAREA";
+export const ACTIVAR_TAREA = "ACTIVAR_TAREA";
+export const RESETEAR_TAREA = "RESETEAR_TAREA";
+export const ORDENAR_TAREA = "ORDENAR_TAREA";
+
+export function agregar_tarea(id_libro, descripcion, tiempo) {
+    return {type: AGREGAR_TAREA, id_libro: id_libro, descripcion: descripcion, tiempo: tiempo}
+}
+
+export function editar_tarea(id_libro, id, descripcion, tiempo) {
+    return {type: EDITAR_TAREA, id_libro: id_libro, id: id, descripcion: descripcion, tiempo: tiempo}
+}
+
+export function eliminar_tarea(id_libro, id) {
+    return {type: ELIMINAR_TAREA, id_libro: id_libro, id: id }
+}
+
+export function activar_tarea(id_libro, id) {
+    return {type: ACTIVAR_TAREA, id_libro: id_libro, id: id }
+}
+
+export function RESETEAR_TAREA(id_libro, id) {
+    return {type: REPETIR_LIBRO, id_libro: id_libro, id: id }
+}
