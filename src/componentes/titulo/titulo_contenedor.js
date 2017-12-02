71f9b7823fbe8b51932c259bb18c7d6574d56bd7
Jonathan Castro <jonatancastro1@gmail.com>
2017-12-02 17:00:23 -0500

    pruebas para v1

--- src/componentes/titulo/titulo_contenedor.js
+++ src/componentes/titulo/titulo_contenedor.js
@@ -2,7 +2,9 @@ import Titulo from './titulo';
 import { routerActions } from 'react-router-redux';
 import { connect } from 'react-redux';
 import { establecer_libro_en_edicion, seleccionar_libro } from "../../redux-acciones/acciones_libros";
-import { establecer_tarea_en_edicion } from "../../redux-acciones/acciones_tareas";
+import { establecer_tarea_activa, establecer_tarea_en_edicion } from "../../redux-acciones/acciones_tareas";
+import { cambiar_estado_tareas } from "../../redux-acciones/acciones_estado";
+import { PAUSADO } from "../../constantes/estados";
 
 const libro_por_defecto = {
     id: '',
@@ -23,6 +25,8 @@ function onBack() {
         dispatch(seleccionar_libro(libro_por_defecto));
         dispatch(establecer_libro_en_edicion(libro_por_defecto));
         dispatch(establecer_tarea_en_edicion(tarea_por_defecto));
+        dispatch(establecer_tarea_activa(''));
+        dispatch(cambiar_estado_tareas(PAUSADO));
         dispatch(routerActions.push('/'));
     }
 }
